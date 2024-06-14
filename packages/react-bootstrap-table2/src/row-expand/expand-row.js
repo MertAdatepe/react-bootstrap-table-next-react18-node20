7102b3a8bc2cdd9316f76b979047a52d47f01157
Orest Furda <orestfurda@MacBook-Air-Orest.local>
2024-06-14 19:41:01 +0300

    Add fix to table to unmount expanded rows when hide

--- packages/react-bootstrap-table2/src/row-expand/expand-row.js
+++ packages/react-bootstrap-table2/src/row-expand/expand-row.js
@@ -1,27 +1,33 @@
 import cs from 'classnames';
-import React from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import { CSSTransition } from 'react-transition-group';
 
-const ExpandRow = ({ children, expanded, onClosed, className, ...rest }) => (
-  <tr>
-    <td className={ cs('reset-expansion-style', className) } { ...rest }>
-      <CSSTransition
-        appear
-        in={ expanded }
-        timeout={ 400 }
-        classNames="row-expand-slide"
-        onExited={ onClosed }
-      >
-        <div>
-          <div className="row-expansion-style">
-            { children }
+const ExpandRow = ({ children, expanded, onClosed, className, ...rest }) => {
+  const nodeRef = useRef(null);
+
+  return (
+    <tr>
+      <td className={ cs('reset-expansion-style', className) } { ...rest }>
+        <CSSTransition
+          appear
+          unmountOnExit
+          in={ expanded }
+          timeout={ 400 }
+          classNames="row-expand-slide"
+          onExited={ onClosed }
+          nodeRef={ nodeRef }
+        >
+          <div ref={ nodeRef }>
+            <div className="row-expansion-style">
+              { children }
+            </div>
           </div>
-        </div>
-      </CSSTransition>
-    </td>
-  </tr>
-);
+        </CSSTransition>
+      </td>
+    </tr>
+  );
+};
 
 ExpandRow.propTypes = {
   children: PropTypes.node,
